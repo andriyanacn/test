e3ed230a28571a06812ec20cd7c9bade98ff5690
andriyanacn <18500041.dwi@sinus.ac.id>
2021-10-08 06:14:49 +0700

    updated middleware

--- middleware/jwt_middleware.go
+++ middleware/jwt_middleware.go
@@ -4,15 +4,29 @@ import (
 	"phone_review/constants"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/dgrijalva/jwt-go"
+	//"github.com/golang-jwt/jwt"
 )
 
-func CreateToken(userID int, name string) (string, error) {
+//const key = "legal"
+
+// func CreateToken(userID int, name string) (string, error) {
+// 	claims := jwt.MapClaims{}
+// 	claims["userID"] = userID
+// 	//claims["name"] = name
+// 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+
+// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+// 	return token.SignedString([]byte(key))
+// }
+
+func CreateToken(adminID int, name string) (string, error) {
 	claims := jwt.MapClaims{}
-	claims["userID"] = userID
+	claims["authorized"] = true
+	claims["adminID"] = adminID
 	claims["name"] = name
 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-	return token.SignedString([]byte(constants.Secret_JWT))
+	return token.SignedString([]byte(constants.SECRET_JWT))
 }
